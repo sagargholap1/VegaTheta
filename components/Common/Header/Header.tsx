ae02568a1845f8255b7fa426296c82d58b054287
Sagar Gholap <sgholap101@gmail.com>
2023-02-15 20:04:02 +0530

    Second Commit

--- components/Common/Header/Header.tsx
+++ components/Common/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import Logo from "../../../public/Logo.png";
 
@@ -7,17 +8,17 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <header className=" ">
-      <nav className="h-[8rem] w-[90%] mx-auto wrapper-2 1450:wrapper 1200:hidden flex items-center justify-between gap-[1rem] ">
+      <nav className="h-[8rem] w-[90%] mx-auto  1200:hidden flex items-center justify-between gap-[1rem] ">
         <div className="w-[17.5rem] h-[6rem]">
-          <Image src={Logo} alt="logo" />
+          <Link href="/"><Image src={Logo} alt="logo" /></Link>
         </div>
         <div>
             <ul className="flex gap-[8rem]">
-                <li className="font-6 text-subtitle-2">Home</li>
-                <li className="font-6 text-subtitle-2">About Us</li>
-                <li className="font-6 text-subtitle-2">Careers</li>
-                <li className="font-6 text-subtitle-2">VegaTheta</li>
-                <li className="font-6 text-subtitle-2">Contact</li>
+                <li className="font-6 text-subtitle-2 hover:text-[#007074] hover:scale-110"><Link href="/">Home</Link></li>
+                <li className="font-6 text-subtitle-2 hover:text-[#007074] hover:scale-110"><Link href="/">About Us</Link></li>
+                <li className="font-6 text-subtitle-2 hover:text-[#007074] hover:scale-110"><Link href="/">Careers</Link></li>
+                <li className="font-6 text-subtitle-2 hover:text-[#007074] hover:scale-110"><Link href="/">VegaTheta</Link></li>
+                <li className="font-6 text-subtitle-2 hover:text-[#007074] hover:scale-110"><Link href="/">Contact</Link></li>
             </ul>
         </div>
       </nav>
--- components/Pages/Homepage/sections/Hero/Hero.tsx
+++ components/Pages/Homepage/sections/Hero/Hero.tsx
@@ -9,7 +9,7 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <section className="wrapper ">
+    <section className=" wrapper  ">
       <div
         className="flex flex-col items-center justify-center gap-[5rem] h-[calc(100vh-8rem)] max-h-[90rem] 1150:h-[90rem] 1150:max-h-max 450:!h-[80rem] overflow-hidden 1100:h-[100%] 1100:py-[5rem] "
         id="hero"
@@ -23,42 +23,50 @@ const Hero = (props: Props) => {
           </div>
           {/* right */}
           <div className="basis-1/2  flex-col gap-[2rem] items-center justify-center flex  ">
-            <h1 className="text-[3rem]">Welcome to VegaTheta</h1>
+            <h1 className="text-[3rem] mb-[4rem]">Welcome to VegaTheta</h1>
             <form
               action=""
               className="bg-[#E3E3E3] flex flex-col p-[4rem] px-[4rem] gap-[2rem] w-[75%] rounded-[2rem] shadow-[25px_20px_40px_5px_rgba(0,0,0,0.3)]"
             >
-              <label htmlFor="Email" className="text-[#4D5959]">
-                Email
-              </label>
-              <input
-                type="Email"
-                placeholder="Enter your Email here"
-                className=" h-[4rem] outline-none  rounded-[.3rem]"
-              />
+              <div className="flex flex-col gap-[1rem]">
+                <label htmlFor="Email" className="text-[#4D5959]">
+                  Email
+                </label>
+                <input
+                  type="Email"
+                  placeholder="Enter your Email here"
+                  className=" h-[4rem] outline-none  rounded-[.3rem] px-[2rem]"
+                />
+              </div>
+              <div className="flex flex-col gap-[1rem]">
               <label htmlFor="Email" className="text-[#4D5959]">
                 Password
               </label>
               <input
                 type="Email"
                 placeholder="Enter your Password here"
-                className=" h-[4rem] outline-none rounded-[.3rem]"
-              />
+                className=" h-[4rem] outline-none rounded-[.3rem] px-[2rem]"
+              /></div>
               <div className="flex justify-between">
                 <div className="flex gap-[1rem]">
-                  <input type="checkbox" name="" id="" className="accent-[#007074]" />
-                  <label htmlFor="" className="text-[#4D5959]">
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    className="accent-[#007074]"
+                  />
+                  <label htmlFor="" className="text-[#737777] font-6">
                     Remember Me
                   </label>
                 </div>
-                <div className="text-[#4D5959] underline hover:text-[#007074]"><Link href="/">
-                Forgot Password?
-                </Link>
+                <div className="text-[#737777] font-6 underline hover:text-[#007074]">
+                  <Link href="/">Forgot Password?</Link>
                 </div>
               </div>
               <button className="bg-[#007074] text-white p-[1rem] w-[10rem] rounded-[1rem]">
                 Sign in
               </button>
+              <div className="text-subtitle-2 font-3">Don’t have an account? <span className="text-[#007074] font-5 hover:underline underline-offset-4 "><Link href="/">Sign Up Here</Link></span></div>
             </form>
             <div className="font-6">OR</div>
 
@@ -67,17 +75,23 @@ const Hero = (props: Props) => {
                 <div className="w-[2rem] h-[2rem]">
                   <Image src={googleIcon} alt="icon" />
                 </div>
-                <span className="text-[1.2rem] text-[#043133] font-5">Sign Up with Google</span>
+                <span className="text-[1.2rem] text-[#043133] font-5">
+                  Sign Up with Google
+                </span>
               </button>
               <button className="flex p-[1rem] items-center justify-center gap-[1rem] border-[2px]">
                 <div className="w-[2rem] h-[2rem]">
                   <Image src={GitIcon} alt="icon" />
                 </div>
-                <span className="text-[1.2rem] text-[#043133] font-5">Sign Up with Github</span>
+                <span className="text-[1.2rem] text-[#043133] font-5">
+                  Sign Up with Github
+                </span>
               </button>
             </div>
 
-            <div className=" text-[1.2rem] font-6">2023 @ All Rights Reserved</div>
+            <div className=" text-[1.2rem] font-6">
+              2023 @ All Rights Reserved
+            </div>
           </div>
         </div>
       </div>
